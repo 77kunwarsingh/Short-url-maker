a5105e7e2203122584f6c3f930ef84dfdcf2cb41
kunwar bhagat singh <kunwarbhagatsingh082002@gmail.com>
2024-09-02 22:57:40 +0530

    changes

--- controllers/url.js
+++ controllers/url.js
@@ -22,6 +22,20 @@ async function handleGetAllUrl(req,res){
     const allUrl = await URL.find({});
     return res.json(allUrl);
 }
+async function handleDeleteUrl(req, res) {
+    try {
+        const url = req.params.url;
+        const result1 = await URL.findOneAndDelete( {url} );
+
+        if (!result1) {
+            return res.status(404).json({ message: "URL not found in the database." });
+        }
+
+        return res.json({ message: `Successfully deleted from the database: ${result1}` });
+    } catch (error) {
+        return res.status(500).json({ message: "An error occurred while deleting the URL.", error: error.message });
+    }
+}
 module.exports = {
-    handleGenerateNewShortURL,handleGetAnalytics,handleGetAllUrl
+    handleGenerateNewShortURL,handleGetAnalytics,handleGetAllUrl,handleDeleteUrl
 } 
--- routes/url.js
+++ routes/url.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const {handleGenerateNewShortURL,handleGetAnalytics,handleGetAllUrl} = require("../controllers/url");
+const {handleGenerateNewShortURL,handleGetAnalytics,handleGetAllUrl , handleDeleteUrl} = require("../controllers/url");
 const router = express.Router();
 router.get("/",handleGetAllUrl)
 router.post('/', handleGenerateNewShortURL);
 router.get('/analytics/:shortId', handleGetAnalytics)
+router.delete("/:redirectUrl", handleDeleteUrl)
 module.exports = router;
